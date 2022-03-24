7db24c6a0d42adbf5a61ddf4cadd0658a3fa6f92
Михаил Егин <egin.1992@mail.ru>
2022-03-24 11:36:55 +0300

    update

--- task.py
+++ task.py
@@ -196,4 +196,4 @@ history = model.fit(
 
 history_frame = pd.DataFrame(history.history)
 history_frame.loc[:, ['loss', 'val_loss']].plot()
-history_frame.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot();
+history_frame.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot()
